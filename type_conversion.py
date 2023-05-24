10b4114f13a88ccda370ef23206705c98174fc5a
UnicornDad8 <unicorndad8@gmail.com>
2023-05-24 14:01:01 -0400

    Type conversion

---
+++ type_conversion.py
@@ -0,0 +1,24 @@
+x = int(input("enter a number x: "))
+y = x + 1
+print(f"x: {x}, y: {y}")
+
+# we have:
+# int( )
+# float( )
+# bool( )
+# str( )
+print("----------------------------------")
+
+print(type(False))
+print(type("hello"))
+print(type(5))
+print(type(1.1))
+print("----------------------------------")
+
+# Falsy values:
+# ""
+# 0
+# None
+print(bool(""))
+print(bool(0))
+print(bool(None))
