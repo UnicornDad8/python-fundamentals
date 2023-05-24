01f5eece37f775c50a6776bf9f2518b895415ec3
UnicornDad8 <unicorndad8@gmail.com>
2023-05-24 12:43:54 -0400

    Escape sequences

---
+++ escape_sequences.py
@@ -0,0 +1,12 @@
+# escape sequences
+
+# \"
+# \'
+# \\
+# \n
+
+# back slash to escape double quotes
+print("write the letter \"A\"")
+print('write the letter \'A\'')
+print("wite one backslash \\")
+print("do a jump\nto the next line")
