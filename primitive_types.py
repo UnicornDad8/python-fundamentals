ec25b8c25b2bb11c43f8eb40fd108782f6684809
UnicornDad8 <unicorndad8@gmail.com>
2023-05-24 10:58:10 -0400

    Primitive types variables

---
+++ primitive_types.py
@@ -0,0 +1,21 @@
+# Variables: Primitive Types...
+
+integer = 10
+print("integer type (int)")
+print(integer)
+print("")
+
+float = 4.9
+print("floating point number type (float)")
+print(float)
+print("")
+
+boolean = False
+print("boolean type (bool)")
+print(boolean)
+print("")
+
+text = "Hi there"
+print("string type (str)")
+print(text)
+print("")
