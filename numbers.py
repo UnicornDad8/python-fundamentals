2f37e518775339e5b0858107ac64755dd0d6feaa
UnicornDad8 <unicorndad8@gmail.com>
2023-05-24 13:29:13 -0400

    Working with numbers and aritmetics

---
+++ numbers.py
@@ -0,0 +1,28 @@
+x = 1  # integer
+y = 1.1  # float
+z = 2 + 3j  # complex number (a + bi)
+
+print(x)
+print(y)
+print(z)
+print("--------------------------------------------")
+
+print(10 + 3)  # sum
+print(10 - 3)  # difference
+print(10 * 3)  # multiplication
+print(10 / 3)  # Division
+print(10 // 3)  # Division (only int)
+print(10 % 3)  # modulus
+print(10 ** 3)  # exponential
+print("--------------------------------------------")
+
+a = 10
+a += 3  # a = a + 3
+print(a)
+
+# a -= 3
+# a *= 3
+# a /= 3
+# a //= 3
+# a %= 3
+# a **= 3
