8fa60504bb4d68d47c5311b7b46b36769717fbc2
UnicornDad8 <unicorndad8@gmail.com>
2023-05-24 13:09:55 -0400

    String methods

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+# String Methods
+
+course = "Python Programming"
+
+print(course)
+print(course.upper())
+print(course.lower())
+print(course.title())  # capitalize the first letter of each word
+print(course.strip())  # removes white space from the begining and end of the string
+print(course.find("Pro"))  # finds the index of "Pro"
+print(course.replace("P", "A"))
+print("Pro" in course)
+print("Swift" not in course)
