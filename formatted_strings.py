004cc3b25af4a7ff885469f2757a3936544fa4ce
UnicornDad8 <unicorndad8@gmail.com>
2023-05-24 12:52:58 -0400

    Formatted strings

---
+++ formatted_strings.py
@@ -0,0 +1,12 @@
+# Formatted Strings
+
+first_name = "Ceci"
+last_name = "Benitez"
+full_name_concatenated = first_name + " " + last_name
+full_name_formatted = f"{first_name} {last_name}"
+formatted_text = f"{len(first_name)} {2 + 2}"
+
+print(full_name_concatenated)
+print(full_name_formatted)
+print("")
+print(formatted_text)
