fa16acc75427f88db57bd41bdd49e25a4b994535
UnicornDad8 <unicorndad8@gmail.com>
2023-05-25 10:17:38 -0400

    Comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,14 @@
+print(10 > 3)  # True
+print(10 >= 3)  # True
+print(10 < 3)  # False
+print(10 <= 3)  # False
+print(10 == 3)  # False
+print(10 != 3)  # True
+print(10 == "10")  # False
+print(10 != "10")  # True
+print("---------------------------------------")
+
+print("bag" > "apple")  # True
+print("bag" == "BAG")  # False
+print(ord("b"))
+print(ord("B"))
