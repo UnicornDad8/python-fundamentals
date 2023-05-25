ee051aa35c6a999f492746db79a603abd771163a
UnicornDad8 <unicorndad8@gmail.com>
2023-05-25 11:54:37 -0400

    Added quiz to primitive types

---
+++ primitive_types/quiz.py
@@ -0,0 +1,19 @@
+# What are the built in primitive types in python?
+'''
+we have:
+- strings
+- numbers
+- booleans
+(numbers can be integers, floats or complex numbers)
+'''
+
+# What do you think we will see on the terminal?
+fruit = "Apple"
+print(fruit[1])  # p
+# print(fruit[1: -1]) => ppl
+
+# What is the result of this expresion?
+10 % 3
+
+# What do you think we will get when we print this expression:
+print(bool("False"))
