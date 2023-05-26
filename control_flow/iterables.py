255e1254fee96e9976278eb1a96f2138d7e9c9d3
UnicornDad8 <unicorndad8@gmail.com>
2023-05-26 15:11:53 -0400

    Learning about iterables

---
+++ control_flow/iterables.py
@@ -0,0 +1,11 @@
+print(type(5))
+print(type(range(5)))
+print("------------------------------------------------")
+
+# Iterable
+for x in "Python":
+    print(x)
+print("------------------------------------------------")
+
+for x in [1, 2, 3, 4, 5]:
+    print(x)
