86efc9e67616c594a3d2420d81c399bf6af025c9
UnicornDad8 <unicorndad8@gmail.com>
2023-05-26 14:48:43 -0400

    for loops

---
+++ control_flow/for_loops.py
@@ -0,0 +1,15 @@
+for number in range(3):
+    print("Attenmpt")
+print("-------------------------------------------------")
+
+for number in range(3):
+    print("Attenmpt", number + 1, (number + 1) * ".")
+print("-------------------------------------------------")
+
+for number in range(1, 4):
+    print("Attenmpt", number, (number) * ".")
+print("-------------------------------------------------")
+
+for number in range(1, 10, 2):
+    print("Attenmpt", number, (number) * ".")
+print("-------------------------------------------------")
