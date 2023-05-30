a621ca7f1fd62434a64e5d36e752b17cbe596598
UnicornDad8 <unicorndad8@gmail.com>
2023-05-30 14:07:15 -0400

    Looping over a list

---
+++ data_structures/looping_over_lists.py
@@ -0,0 +1,8 @@
+letters = ["a", "b", "c"]
+
+for letter in letters:
+    print(letter)
+print("-----------------------------------------------------")
+
+for index, letter in enumerate(letters):
+    print(index, letter)
