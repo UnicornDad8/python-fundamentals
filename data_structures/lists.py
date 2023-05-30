3eb7c24132848aa5017afaf5f5a1a8eba0a82c7f
UnicornDad8 <unicorndad8@gmail.com>
2023-05-30 12:43:40 -0400

    Started with lists

---
+++ data_structures/lists.py
@@ -0,0 +1,16 @@
+# Lists
+
+letters = ["a", "b", "c"]
+list_of_zeros = [0] * 5
+combined = list_of_zeros + letters
+numbers = list(range(1, 10))
+chars = list("Hello world")
+matrix = [[0, 1], [2, 3]]
+
+print(list_of_zeros)
+print(combined)
+print(numbers)
+print(chars)
+print(matrix)
+print(len(chars))
+print(len(matrix))
