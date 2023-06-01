2ad42d5a60376439e39f1376f3b3a0ec27c9472d
UnicornDad8 <unicorndad8@gmail.com>
2023-06-01 10:34:04 -0400

    Working with stacks

--- data_structures/zip.py
+++ data_structures/zip.py
@@ -1,6 +1,8 @@
 list1 = [1, 2, 3]
 list2 = [10, 20, 30]
 
+# in this case using map() or filter() is not enough because we want to
+# combine two lists: list1 and list2, so we use the zip method
 print(list(zip(list1, list2)))
 
 # we can work not only with lists but with any iterable
