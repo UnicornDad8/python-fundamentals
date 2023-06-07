d5fc945fb1fe64b17d4a8a75e37b1e2fdb21ea93
UnicornDad8 <unicorndad8@gmail.com>
2023-06-07 11:26:24 -0400

    Working with exceptions in python

---
+++ exceptions/exceptions.py
@@ -0,0 +1,6 @@
+numbers = [1, 2]
+print(numbers[3])  # this error throws an exception
+print("---------------------------------------------------")
+
+age = int(input("Age: "))  # if we enter any other type than int it will
+# throw an exception
