6ec8a195759ebba4e37125412a98fa3b502dd580
UnicornDad8 <unicorndad8@gmail.com>
2023-06-13 11:08:00 -0400

    Comparing two objects of the same class

---
+++ classes/comparing_objects.py
@@ -0,0 +1,19 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
+
+point1 = Point(1, 2)
+point2 = Point(1, 2)
+print(point1 == point2)
+
+point1 = Point(10, 20)
+print(point1 > point2)
+print(point1 < point2)
