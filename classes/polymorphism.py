d0f6dfc8abbf34f9f2a0d12d0eebb1136051b119
UnicornDad8 <unicorndad8@gmail.com>
2023-06-15 08:26:51 -0400

    An example of polymorphism

---
+++ classes/polymorphism.py
@@ -0,0 +1,32 @@
+# In this example the "draw" method is taking many forms or giving different
+# results every time we execute it, this case is known as polymorphism
+from abc import ABC, abstractmethod
+
+
+class UIControl(ABC):
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+class TextBox(UIControl):
+    def draw(self):
+        print("TextBox")
+
+
+class DropDownList(UIControl):
+    def draw(self):
+        print("DropDownList")
+
+
+def draw(controls):
+    for control in controls:
+        control.draw()
+
+
+ddl = DropDownList()
+print(isinstance(ddl, UIControl))
+
+textbox = TextBox()
+
+draw([ddl, textbox])
